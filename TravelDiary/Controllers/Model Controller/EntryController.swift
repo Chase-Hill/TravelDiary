8e9b99a66bfacee1f9317f518daa89a829e7ecaf
Chase <chaseaohill@gmail.com>
2023-02-09 13:11:12 -0500

    Finished model controller

--- TravelDiary/Controllers/Model Controller/EntryController.swift
+++ TravelDiary/Controllers/Model Controller/EntryController.swift
@@ -10,24 +10,42 @@ import Foundation
 class EntryController {
     
     // MARK: - Singleton
+    /// Singleton: To ensure that we are interacting with the *SAME* instance of our *entries* array we created a singleton to pass around this specific instance of our **EntryController**
     static let sharedInstance = EntryController()
+    
     // MARK: - Source of Truth
+    ///Source of Truth: The single location we will store our **Entry ** data.
     var entries: [Entry] = [] // This is the ARRAY that I am going to move around the app to control my data.
+    
     // MARK: - CRUD functions
-    func create() {
-        
+    func createEntry(title: String, addy: String, body: String) {
+        let entry = Entry(title: title, address: addy, body: body)
+        //Adding it to the Source of Truth
+        entries.append(entry)
+        save()
     }
     
-    func update() {
-        
+    func update(entryToUpdate: Entry, newTitle: String, newAddress: String, newBody: String) {
+        entryToUpdate.title = newTitle
+        entryToUpdate.address = newAddress
+        entryToUpdate.body = newBody
+        save()
     }
     
-    func delete() {
-        
+    func delete(entryToDelete: Entry) {
+        guard let index = entries.firstIndex(of: entryToDelete) else {return}
+        entries.remove(at: index)
+        save()
     }
     
     // MARK: - Persistence
     
+    func save() {
+        
+    }
     
+    func load() {
+        
+    }
 
 } // End of Class
--- TravelDiary/Model/Entry.swift
+++ TravelDiary/Model/Entry.swift
@@ -8,15 +8,22 @@
 import Foundation
 
 class Entry {
-    let title: String
-    let address: String
+    var title: String
+    var address: String
     let entryDate: Date
-    let body: String
+    var body: String
     
     init(title: String, address: String, entryDate: Date = Date(), body: String) {
         self.title = title
         self.address = address
         self.entryDate = entryDate
         self.body = body
+    }
+}
+
+extension Entry: Equatable { // Adopting
+    // Conforming
+    static func == (lhs: Entry, rhs: Entry) -> Bool {
+        return lhs.entryDate == rhs.entryDate // Here we don't need to add anything else because the difference in entryDate is enough of a change for us.
     }
 }
