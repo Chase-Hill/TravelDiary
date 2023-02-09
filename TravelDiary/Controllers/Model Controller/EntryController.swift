b6eb03d1a3a3bdde3d378363da7bed49a4c61ea0
Chase <chaseaohill@gmail.com>
2023-02-09 12:10:17 -0500

    Created model and model controller. Organized folders.

---
+++ TravelDiary/Controllers/Model Controller/EntryController.swift
@@ -0,0 +1,33 @@
+//
+//  EntryController.swift
+//  TravelDiary
+//
+//  Created by Chase on 2/9/23.
+//
+
+import Foundation
+
+class EntryController {
+    
+    // MARK: - Singleton
+    static let sharedInstance = EntryController()
+    // MARK: - Source of Truth
+    var entries: [Entry] = [] // This is the ARRAY that I am going to move around the app to control my data.
+    // MARK: - CRUD functions
+    func create() {
+        
+    }
+    
+    func update() {
+        
+    }
+    
+    func delete() {
+        
+    }
+    
+    // MARK: - Persistence
+    
+    
+
+} // End of Class
